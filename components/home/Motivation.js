27825c1ee07e89be3a28fd90c37b85a7d7d36c3d
aankansah <47257672+augedoo@users.noreply.github.com>
2022-03-29 00:09:32 +0000

    Complete motivation section

--- components/home/Motivation.js
+++ components/home/Motivation.js
@@ -4,10 +4,10 @@ import Button from '../ui/Button';
 const Motivation = () => {
 	return (
 		<div className='banner h-[350px] sm:w-[90%] mx-auto lg:mx-0 lg:w-auto mb-10 amt-content-padded-left'>
-			<div className='relative h-full amt-content-aligned-left'>
+			<div className='relative h-full '>
 				<div className='relative z-10 w-full h-full overflow-hidden'>
 					{/* Overlay */}
-					<div className='absolute z-20 w-full h-full opacity-40 bg-gradient-to-r from-primary-500 via-primary-500 to-secondary-700'></div>
+					<div className='absolute z-20 w-full h-full opacity-40 bg-gradient-to-r from-secondary-700 via-primary-500 to-primary-500'></div>
 
 					<img
 						src={crowd}
@@ -16,8 +16,10 @@ const Motivation = () => {
 					/>
 				</div>
 
-				<div className='absolute inset-x-0 inset-y-0 z-20 flex flex-col items-center justify-center h-full gap-4 transform lg:left-auto lg:right-0 lg:items-end lg:translate-x-1/2 text-secondary-700'>
-					<h1 className='pb-6 text-4xl font-bold'>Together we can Do more </h1>
+				<div className='absolute inset-x-0 inset-y-0 z-20 flex flex-col items-center justify-center h-full gap-4 text-white transform lg:left-auto lg:right-0 lg:items-end lg:translate-x-[50px]  xl:translate-x-1/3 lg:text-secondary-600'>
+					<h1 className='max-w-xs pb-10 mx-auto text-4xl font-bold text-center lg:max-w-none'>
+						Together we can Do more{' '}
+					</h1>
 
 					<div className='flex justify-center lg:justify-end'>
 						<Button
